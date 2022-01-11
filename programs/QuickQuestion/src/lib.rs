ec7992ecd825fc90945b382623855729900561ca
Colby Harrison <colbyharrison@Colbys-Mac-mini.home>
2022-01-11 12:47:35 -0800

    update pubkey, add basic structs

--- programs/QuickQuestion/src/lib.rs
+++ programs/QuickQuestion/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3nwtVSUXMNeFYDA4dXrSLb8qYwMXwm1CaEoho5PRW2aH");
 
 #[program]
 pub mod quick_question {
@@ -12,3 +12,23 @@ pub mod quick_question {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+pub struct Bounty {
+    pub question: String,
+    pub amount: u64,
+    pub open_time: u64,
+    pub answers: Vec<Answer>,
+    pub state: State,
+    pub questioner_key: Pubkey,
+}
+
+pub struct Answer {
+    pub response: String,
+    pub reponder_key: Pubkey,
+    was_accepted: bool,
+}
+
+pub enum State {
+    open,
+    closed,
+}
