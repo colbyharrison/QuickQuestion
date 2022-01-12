57d9e456709a5ae548005f69799336ed819a64eb
Colby Harrison <colbyharrison@Colbys-Mac-mini.home>
2022-01-12 08:36:01 -0800

    update accounts

--- programs/QuickQuestion/src/lib.rs
+++ programs/QuickQuestion/src/lib.rs
@@ -13,8 +13,10 @@ pub mod quick_question {
 #[derive(Accounts)]
 pub struct Initialize {}
 
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct Bounty {
-    pub question: String,
+    pub title: String,    //limit to 50 chars
+    pub question: String, //limit to 2500 chars
     pub amount: u64,
     pub open_time: u64,
     pub answers: Vec<Answer>,
@@ -22,12 +24,14 @@ pub struct Bounty {
     pub questioner_key: Pubkey,
 }
 
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct Answer {
-    pub response: String,
+    pub response: String, //limit to 2500 chars
     pub reponder_key: Pubkey,
     was_accepted: bool,
 }
 
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
 pub enum State {
     open,
     closed,
